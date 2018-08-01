af12dd0375d72b8b0c5d79cf5f15f6fa2b8c764d
Tim Hawes <tim@selfdidactic.com>
2018-07-31 21:27:07 -0400

    Progress

---
+++ chapter04/prompt/tests/src/tests.cpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2018 Anonymous
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include "catch.hpp"
+
+
+TEST_CASE( "Tests for prompt", "[prompt]" ) {
+  SECTION ( "Testing prompt" ) {
+  }
+}
+
